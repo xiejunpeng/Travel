1e8f72b52a226d9ca9a647408922c105820c9637
xiejunpeng <984061430@qq.com>
2019-05-11 15:02:19 +0800

    add header

--- my-project/build/webpack.base.conf.js
+++ my-project/build/webpack.base.conf.js
@@ -22,11 +22,14 @@ module.exports = {
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
+
+  // 起别名的配置
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src'),   
+      'styles': resolve('src/assets/styles'),   
     }
   },
   module: {
---
+++ my-project/src/pages/home/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe605;</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeHeader"
+};
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'   //前面要加个波浪线
+.header {
+  display: flex;
+  background: $bgColor;
+  color: #fff;
+  line-height: 0.86rem;
+
+  .header-left {
+    width: 0.64rem;
+    float: left;
+  }
+
+  .header-input {
+    flex: 1;
+    background: #fff;
+    color: #ECEAEB;
+    height: 0.64rem;
+    line-height: 0.64rem;
+    margin-top: 0.12rem;
+    margin-right: 0.2rem;
+    border-radius: 0.1rem;
+  }
+
+  .header-right {
+    width: 0.1 0.24rem;
+    float: right;
+    text-align: center;
+  }
+}
+</style>
+
