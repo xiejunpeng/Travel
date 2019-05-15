7cb5fdeca799763d730e60c5b8d8c5638495ec1e
xiejunpeng <984061430@qq.com>
2019-05-15 17:57:00 +0800

    add Search to city

--- my-project/src/pages/city/City.vue
+++ my-project/src/pages/city/City.vue
@@ -1,14 +1,22 @@
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 <script>
 import CityHeader from "./components/Header";
+import CitySearch from "./components/Search";
 export default {
   name: "City",
-  components: {     //components   里面有S的
-    CityHeader
+  components: {
+    //components   里面有S的
+    CityHeader,
+    CitySearch
   }
 };
 </script>
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+    
+</style>
 
