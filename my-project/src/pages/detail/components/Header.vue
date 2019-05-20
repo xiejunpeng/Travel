143ce60c46a715caf9c1258820081397a7e1cb9f
xiejunpeng <984061430@qq.com>
2019-05-20 21:40:31 +0800

    fix problem

--- my-project/src/pages/detail/components/Header.vue
+++ my-project/src/pages/detail/components/Header.vue
@@ -38,6 +38,10 @@ export default {
   },
   activated() {
     window.addEventListener("scroll", this.handleScroll);
+  },
+  // 对全局事件的解绑
+  deactivated() {
+    window.removeEventListener("scroll", this.handleScroll);
   }
 };
 </script>
