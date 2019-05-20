d72bcd987f0434fe4cb67107fc246065388d236d
xiejunpeng <984061430@qq.com>
2019-05-20 20:43:00 +0800

    header animation

--- my-project/src/common/gallary/Gallary.vue
+++ my-project/src/common/gallary/Gallary.vue
@@ -20,8 +20,8 @@ export default {
       type: Array,
       default() {
         return [
-          "http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg",
-          "http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg"
+          "http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg",
+          "http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png"
         ];
       }
     }
@@ -38,7 +38,7 @@ export default {
   },
   methods: {
     handleGallaryClick() {
-      this.$emit("close")
+      this.$emit("close");
     }
   }
 };
@@ -63,7 +63,7 @@ export default {
   .wrapper {
     height: 0;
     width: 100%;
-    padding-bottom: 60%;
+    padding-bottom: 100%;
 
     .gallary-img {
       display: block;
--- my-project/src/main.js
+++ my-project/src/main.js
@@ -4,18 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
+import VueAwesomeSwiper from 'vue-awesome-swiper'   //1)轮播图的js
 import store from "./store"
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
-import 'swiper/dist/css/swiper.css'
+import 'swiper/dist/css/swiper.css'    //轮播图的js
 
 Vue.config.productionTip = false
 
 fastClick.attach(document.body)  // 解决移动端有个300毫秒点击延迟的问题  
 /* eslint-disable no-new */
-Vue.use(VueAwesomeSwiper)
+Vue.use(VueAwesomeSwiper)    //2)使用轮播图js
 new Vue({
   el: '#app',//挂载点指的是index.html
   router,  //路由根据网址的不同,返回不同的内容给用户
